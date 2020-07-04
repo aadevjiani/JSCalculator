7235a59e5544d317e5682be6ccd9aeaa4e4e833d
aadevjiani <57323312+aadevjiani@users.noreply.github.com>
2020-07-04 18:28:29 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 // Assignment by Ali Abbas Devjiani
 // Assignment No. 7 - Calculator
-// Still working to finalize - motivated to continue and increase the pace
+// Calculator
+function getNumber(num){
+    // console.log(num) 
+    var result = document.getElementById("result")
+    result.value += num      // + sign in this line will help in concatenation otherwise value of variable will be replaced / updated
+  }
+  
+  function clearResult(){
+    var result = document.getElementById("result")
+    result.value = " "
+  }
+  
+  function getResult(){
+    var result = document.getElementById("result")
+    //console.log(result.value)
+    result.value = eval(result.value)
+  }
